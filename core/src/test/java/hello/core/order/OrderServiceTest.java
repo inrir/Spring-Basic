d45c82b78dcefa212fbaa85756c026bf23a86b1c
inrir <kimms5617@naver.com>
2022-03-30 00:15:25 +0900

    finish section2

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L; // primitive 타입을 사용해도 되지만, wrapper 타입을 쓰는 이유는 null 넣을 수도 있어서
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
